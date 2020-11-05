913caea59dae516b206551b108c928ad08640327
shubhsk88 <shubham88singh@gmail.com>
2020-11-05 14:00:26 +0530

    Fixing the logic of working game

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import PlayContext from './context/PlayContext';
 function App() {
   const [board, setBoard] = useState(() => createArray())
   const [turn, setTurn] = useState(0);
+  const [result, setResult] = useState(null);
+  const [isOver, setIsOver] = useState(false);
   function insert(row, col, value) {
     board[row][col] = value;
     setBoard(board)
@@ -22,6 +24,17 @@ function App() {
       const rowIdx = getRowIdx(board, col);
       insert(rowIdx, col, player)
       setTurn(turn + 1);
+      if (checkWinner(player, board)) {
+        setIsOver(true)
+        if (player === 1) {
+          setResult('Yay Player 1 Wins')
+        }
+        else if (player === 2) {
+          setResult('Yay Player 2 Wins')
+        }
+      }
+
+
 
     }
 
@@ -31,7 +44,9 @@ function App() {
   return (
     <div className="flex flex-col justify-center h-screen items-center">
       <div className="py-10 text-6xl ">Connect 4 game</div>
+      
       <PlayContext.Provider value={play}>
+
         <Board board={board} />
       </PlayContext.Provider>
       <button className="px-4 py-2 bg-purple-500 hover:bg-purple-700 text-white rounded shadow-lg my-6 text-4xl">Restart</button>
